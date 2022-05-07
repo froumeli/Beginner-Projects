d52ac302b36ec147875fbef077e324688a31292b
Fotis Roumeliotis <fotisroumeli@gmail.com>
2022-05-08 00:22:26 +0300

    Finished 2nd degree polynomial project

--- 2nd_deg_pol_root.py
+++ 2nd_deg_pol_root.py
@@ -17,8 +17,12 @@ while True:
     c = coef_list[2]
     if a == 0:
         if b == 0:
-            print("This is not a polynomial.")
-            continue
+            print(f"This is the constant polynomial {c}.")
+            othpol = fc.other_pol()
+            if othpol == 'y':
+                continue
+            else:
+                break
         else:
             if c == 0:
                 print(f"The polynomial is {b}*x and its root is x = 0.")
@@ -34,6 +38,39 @@ while True:
                     continue
                 else:
                     break
-    else:
-        print            
-    Δ = b^2 - 4*a*c
+    else :           
+        Δ = b**2 - 4*a*c
+        print(Δ)
+        if Δ < 0 :
+            if a < 0:
+                print("The polynomial has no real roots and is negative for every x in R.")
+                othpol = fc.other_pol()
+                if othpol == 'y':
+                    continue
+                else:
+                    break
+            elif a > 0 :
+                print("The polynomial has no real roots and is positive for every x in R.")
+                othpol = fc.other_pol()
+                if othpol == 'y':
+                    continue
+                else:
+                    break
+        elif Δ == 0:
+            print(f"The polynomial has a double root, which is {(-b)/a}.")
+            othpol = fc.other_pol()
+            if othpol == 'y':
+                continue
+            else:
+                break
+        elif Δ > 0:
+            x1 = ((-b) + Δ**(1/2)) / (2*a)
+            x2 = ((-b) - Δ**(1/2)) / (2*a)
+            print(f"The polynomial has two roots which are:\nx1: {x1}\nx2: {x2}")
+            othpol = fc.other_pol()
+            if othpol == 'y':
+                continue
+            else:
+                break
+
+print("Shutting down...")
